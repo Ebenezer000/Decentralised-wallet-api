d48f1fd751c8d5bb2f06b03fcd869faaa4ec57ab
Ebenezer Akpas <web3devmain@proton.me>
2024-07-29 18:44:12 +0100

    add phase to account

---
+++ wallet/evm/__init__.py
@@ -0,0 +1,24 @@
+from web3 import Web3
+from hexbytes import HexBytes
+from web3.middleware import construct_sign_and_send_raw_middleware
+
+
+def phrase_to_account(chain_provider, phrase) -> dict:
+    """
+    Function to turn seed phrase into account dictionary
+    Args:
+        chain[str]: Name of current chain
+        phrase[str]: Seed phrase of account
+    Returns:
+        dict: account attached to seed phrase      
+    """
+    w3 = Web3(Web3.HTTPProvider(f'{chain_provider}'))
+    account = ""
+
+    w3.eth.account.enable_unaudited_hdwallet_features()
+    acc = w3.eth.account.from_mnemonic(phrase)
+    account = {
+        "address": str(acc.address),
+        "key": HexBytes.hex(acc.key),
+    }
+    return account
